5847548920231f0eb7c60d3e4060940846d79718
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-08-26 21:55:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/GooseTrack">
+        <BrowserRouter basename="/react_node_fs_68_Hatrison">
           <App />
         </BrowserRouter>
       </PersistGate>
